fd74cdddc0c1cdc20a5f0865636966e89fcd839b
minmaxmarty <aregmkhitaryan@gmail.com>
2025-07-11 22:27:11 +0300

    add homework from last semester

--- enigma.py
+++ enigma.py
@@ -1,151 +1,146 @@
-
 import json
 import sys
 
-# ----------------------Consts-------------------------#
-CORRECT_CONFIG_FILE_CONTENTS = ["hash_map", "wheels", "reflector_map"]
-HASHMAP, WHEELS, REFLECTOR = CORRECT_CONFIG_FILE_CONTENTS
-ADD = "add"
-SUBTRACT = "sub"
-EMPTY = ""
-CONFIG_FLAG = "-c"
-INPUT_FLAG = "-i"
-OUTPUT_FLAG = "-o"
-W1_RESET_VALUE = 1
-W2_CHECK = 2
-W3_CHECK_ONE = 10
-W3_CHECK_TWO = 3
-FIRST_W3_VALUE = 10
-SECOND_W3_VALUE = 5
-THIRD_W3_VALUE = 0
-W1_COEFFICIENT = 2
-W2_MULTIPLIER = 2
-MOD_VALUE = 26
-W1_UPPER_LIMIT = 8
-GENERAL_ERROR_MESSAGE = "The enigma script has encountered an error"
+# Constants
 USAGE_ERROR_MESSAGE = "Usage: python3 enigma.py -c <config_file> -i <input_file> -o <output_file>"
+ENIGMA_ERROR_MESSAGE = "The enigma script has encountered an error"
+NEWLINE = "\n"
 
-# -------------------- exceptions -------------------- #
-class JSONFileException (IOError):
-    pass
+# Enigma settings
+WHEEL_MAX = 8
+WHEEL_STEP_2_EVEN_MULTIPLIER = 2
+WHEEL_STEP_2_ODD_DECREMENT = 1
+WHEEL_STEP_3_DIV_10_RESET = 10
+WHEEL_STEP_3_DIV_3_RESET = 5
+WHEEL_STEP_3_DEFAULT = 0
+ALPHABET_LENGTH = 26
+STEP_2_DEFAULT_INCREMENT = 1
 
-# ---------------------- class ---------------------- #
+class JSONFileException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+def get_first_key_by_value(map, value):
+    try:
+        return [k for k, v in map.items() if v == value][0]
+    except Exception as e:
+        raise e
+
+
 class Enigma:
     def __init__(self, hash_map, wheels, reflector_map):
         self.hash_map = hash_map
         self.wheels = wheels
         self.reflector_map = reflector_map
-        self.reverse_hash = {value : key for key, value in self.hash_map.items()}
 
+    def encrypt_letter(self, c):
+        if c not in self.hash_map:
+            return c, False
+
+        i = self.hash_map[c]
+        remainder = ((2 * self.wheels[0]) - self.wheels[1] + self.wheels[2]) % ALPHABET_LENGTH
+        i += remainder if remainder != 0 else STEP_2_DEFAULT_INCREMENT
+        i %= ALPHABET_LENGTH
+        
+        c1 = get_first_key_by_value(self.hash_map, i)
+        c2 = self.reflector_map[c1]
+        i = self.hash_map[c2]
+        i -= remainder if remainder != 0 else STEP_2_DEFAULT_INCREMENT
+        i %= ALPHABET_LENGTH
+        
+        return get_first_key_by_value(self.hash_map, i), True
+
+    def forward_wheels(self, encrypted_count):
+        self.wheels[0] += 1
+        if self.wheels[0] > WHEEL_MAX:
+            self.wheels[0] = 1
+
+        if encrypted_count % 2 == 0:
+            self.wheels[1] *= WHEEL_STEP_2_EVEN_MULTIPLIER
+        else:
+            self.wheels[1] -= WHEEL_STEP_2_ODD_DECREMENT
+
+        if encrypted_count % 10 == 0:
+            self.wheels[2] = WHEEL_STEP_3_DIV_10_RESET
+        elif encrypted_count % 3 == 0:
+            self.wheels[2] = WHEEL_STEP_3_DIV_3_RESET
+        else:
+            self.wheels[2] = WHEEL_STEP_3_DEFAULT
 
     def encrypt(self, message):
-        encrypted_message = EMPTY
-        counter = 0
-        wheels_copy = self.wheels.copy()
+        if not message:
+            return ""
+        original_wheels = self.wheels.copy()
+        encrypted_word = ''
+        encrypted_count = 0
         for c in message:
-            encrypted_char = encrypt_char(c, self.hash_map, wheels_copy, self.reflector_map, self.reverse_hash)
-            if encrypted_char != c:
-                counter = counter + 1
-            encrypted_message = encrypted_message + encrypted_char
-            change_wheels(wheels_copy, counter)
-        return encrypted_message
+            new_letter, if_encrypted = self.encrypt_letter(c)
+            if if_encrypted:
+                encrypted_count += 1
+            encrypted_word += new_letter
+            self.forward_wheels(encrypted_count)
+        
+        self.wheels = original_wheels
+        return encrypted_word
 
-# -------------------- functions -------------------- #
+
 def load_enigma_from_path(path):
     try:
-        with open(path, "r") as inputFile:
-            data = json.load(inputFile)
-            for name in CORRECT_CONFIG_FILE_CONTENTS:
-                if name not in data:
-                    raise JSONFileException
-            return Enigma(data[HASHMAP], data[WHEELS], data[REFLECTOR])
-    except (IOError, ValueError):
-        raise JSONFileException
+        with open(path, 'r') as file:
+            data = json.load(file)
+        return Enigma(data["hash_map"], data["wheels"], data["reflector_map"])
+    except Exception as e:
+        raise JSONFileException("Failed to load file")
 
-def get_new_i(wheels, i, operation = ADD):
-    w1, w2, w3 = wheels
-    if operation == SUBTRACT:
-        op = -1
-    else:
-        op = 1
-    check = ((W1_COEFFICIENT * w1) - w2 + w3) % MOD_VALUE
-    if check !=  0:
-        i = i + op * check
-    else:
-        i = i + op * 1
-    return mod_i(i)
 
-def mod_i(i):
-    return i % MOD_VALUE
+def exit_with_message(message):
+    print(message, file=sys.stderr)
+    sys.exit(1)
 
-def change_wheels(wheels, counter):
-    w1, w2, w3 = wheels
-    w1 = w1 + 1
-    if w1 > W1_UPPER_LIMIT:
-        w1 = W1_RESET_VALUE
-    if counter % W2_CHECK == 0:
-        w2 = w2 * W2_MULTIPLIER
-    else:
-        w2 = w2 - 1
-    if counter % W3_CHECK_ONE == 0:
-        w3 = FIRST_W3_VALUE
-    elif counter % W3_CHECK_TWO == 0:
-        w3 = SECOND_W3_VALUE
-    else:
-        w3 = THIRD_W3_VALUE
 
-    wheels[:] = [w1, w2, w3]
+def get_args():
+    args = sys.argv
+    config_path, input_path, output_path = None, None, None
+    for i in range(1, len(args), 2):
+        if args[i] not in {'-i', '-o', '-c'}:
+            exit_with_message(USAGE_ERROR_MESSAGE)
+        if i + 1 >= len(args):
+            exit_with_message(USAGE_ERROR_MESSAGE)
+        
+        flag, path = args[i], args[i + 1]
+        if flag == '-c':
+            config_path = path
+        if flag == '-i':
+            input_path = path
+        if flag == '-o':
+            output_path = path
+    
+    if not config_path or not input_path:
+        exit_with_message(USAGE_ERROR_MESSAGE)
+    
+    return config_path, input_path, output_path
 
-def encrypt_char(c, hash_map, wheels, reflector_map, reverse_hash):
-    if c.isalpha() and c.islower():
-        i = hash_map[c] # 1
-        i = get_new_i(wheels, i, ADD) # 2 + 3
-        c1 = reverse_hash[i] # 4
-        c2 = reflector_map[c1] # 5
-        i = hash_map[c2] # 6
-        i = get_new_i(wheels, i, SUBTRACT) # 7 + 8
-        c3 = reverse_hash[i] # 9
-        return c3
-    else:
-        return c
 
-def main(config_file, input_file, output_file = None):
+def main():
+    config_path, input_path, output_path = get_args()
     try:
-        enigma = load_enigma_from_path(config_file)
-        with open(input_file, 'r') as f:
-            lines = f.readlines()
-        if output_file is not None:
-            with open(output_file, 'w') as output:
-                for line in lines:
-                    output.write(enigma.encrypt(line))
+        enigma = load_enigma_from_path(config_path)
+        output_file_content = ""
+        with open(input_path, "r") as file:
+            for line in file:
+                clean_line = line.rstrip('\n')
+                output_file_content += enigma.encrypt(clean_line)
+                output_file_content += NEWLINE
+        
+        if output_path:
+            with open(output_path, "w") as file:
+                file.write(output_file_content)
         else:
-            for line in lines:
-                print(enigma.encrypt(line), end=EMPTY)
-    except (IOError, JSONFileException):
-        print_error_message_and_exit()
+            print(output_file_content)
+    except Exception as e:
+        exit_with_message(ENIGMA_ERROR_MESSAGE)
 
-def print_error_message_and_exit():
-    print(GENERAL_ERROR_MESSAGE)
-    exit(1)
 
-def print_usage_message_into_stderr():
-    print(USAGE_ERROR_MESSAGE, file = sys.stderr)
-
-# ------------------ run as script only ------------------ #
-if __name__ == "__main__": # here I just write my script
-    # python3 enigma.py -c <config_file> -i <input_file> -o <output_file>
-    if len(sys.argv) > 7:
-        print_usage_message_into_stderr()
-        exit(1)
-    try:
-        config_file_index = sys.argv.index(CONFIG_FLAG) + 1
-        input_file_index = sys.argv.index(INPUT_FLAG) + 1
-    except ValueError:
-        print_usage_message_into_stderr()
-        exit(1)
-
-    if OUTPUT_FLAG in sys.argv:
-        output_file_index = sys.argv.index(OUTPUT_FLAG) + 1
-        main(sys.argv[config_file_index], sys.argv[input_file_index], sys.argv[output_file_index])
-    else:
-        main(sys.argv[config_file_index], sys.argv[input_file_index])
+if __name__ == "__main__":
+    main()
